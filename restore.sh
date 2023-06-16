d826b6f17cbc4c8f909c0e882634a400ff667af8
Christopher <github.bigbeartechworld@emailcove.com>
2023-06-16 14:05:14 -0500

    Add install step for permissions and to restart scrypted

--- restore.sh
+++ restore.sh
@@ -24,6 +24,9 @@ unzip "$latest_file" -d "./.scrypted-restore"
 # Rsync the latest file to the SSH server with password authentication
 rsync -avz "./.scrypted-restore/.scrypted/" "$ssh_server:$remote_directory/.scrypted"
 
+# Set permissions and remove node_modules and reinstall
+ssh "$ssh_server" "chown -R root:root $remote_directory/.scrypted && cd $remote_directory/.scrypted && rm -rf node_modules && npm install && service scrypted restart"
+
 # Remove the .scrypted-restore directory
 rm -rf ./.scrypted-restore
 
